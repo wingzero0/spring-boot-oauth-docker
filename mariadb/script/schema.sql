1ba1e47fc51a5888e7bcfeb5d4cfe8f0943443cb
wingzero0 <wingzzz2003@hotmail.com>
2019-03-06 16:30:54 +0800

    modify library dependency and update db driver, connection str

--- mariadb/script/schema.sql
+++ mariadb/script/schema.sql
@@ -52,3 +52,14 @@ create table oauth_approvals (
 	expiresAt DATETIME,
 	lastModifiedAt DATETIME
 );
+
+insert into oauth_client_details(client_id, resource_ids, client_secret, scope, authorized_grant_types, authorities, access_token_validity, refresh_token_validity)
+ values ('spring-security-oauth2-read-client', 'resource-server-rest-api',
+   /*spring-security-oauth2-read-client-password1234*/'$2a$04$WGq2P9egiOYoOFemBRfsiO9qTcyJtNRnPKNBl5tokP7IP.eZn93km',
+	  'read', 'password,authorization_code,refresh_token,implicit', 'user', 10800, 2592000);
+
+
+insert into oauth_client_details(client_id, resource_ids, client_secret, scope, authorized_grant_types, authorities, access_token_validity, refresh_token_validity)
+ values ('spring-security-oauth2-read-write-client', 'resource-server-rest-api',
+  /*spring-security-oauth2-read-write-client-password1234*/'$2a$04$soeOR.QFmClXeFIrhJVLWOQxfHjsJLSpWrU1iGxcMGdu.a5hvfY4W',
+	 'read,write', 'password,authorization_code,refresh_token,implicit', 'user', 10800, 2592000);
