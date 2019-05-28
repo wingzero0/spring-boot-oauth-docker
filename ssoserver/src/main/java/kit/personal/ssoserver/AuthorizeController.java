59af87fdf6600e43403a52cfba07b9dae030b5cd
wingzero0 <wingzzz2003@hotmail.com>
2019-05-28 03:37:52 +0000

    try to implement approve form

--- ssoserver/src/main/java/kit/personal/ssoserver/AuthorizeController.java
+++ ssoserver/src/main/java/kit/personal/ssoserver/AuthorizeController.java
@@ -1,5 +1,7 @@
 package kit.personal.ssoserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.provider.AuthorizationRequest;
 import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpoint;
 import org.springframework.security.web.csrf.CsrfToken;
@@ -13,17 +15,43 @@ import org.springframework.web.util.HtmlUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
 import java.util.Map;
 
 //@FrameworkEndpoint
 @Controller
 @SessionAttributes("authorizationRequest")
 public class AuthorizeController {
+    private static Logger LOG = LoggerFactory.getLogger(AuthorizeController.class);
 
     @RequestMapping("/oauth/confirm_access")
     public String getAccessConfirmation(Map<String, Object> model, HttpServletRequest request)
             throws Exception {
+        LOG.debug("dumping confirm_access model key:value");
+        model.forEach((key, value) -> LOG.debug(key + ":" + value.toString()));
 
-        return "test";
+        LOG.debug("dumping confirm_access HttpServletRequest ParameterMap key:value");
+        Map<String,String[]> requestMap = request.getParameterMap();
+        requestMap.forEach((key, value) -> LOG.debug(key + ":" + value.toString()));
+
+        LOG.debug("dumping confirm_access HttpServletRequest AttributeNames key:value");
+        Enumeration<String> requestAttributeNames =  request.getAttributeNames();
+        while(requestAttributeNames.hasMoreElements()){
+            String attrName = requestAttributeNames.nextElement();
+            LOG.debug(attrName + ":" + request.getAttribute(attrName).toString());
+        }
+
+        AuthorizationRequest authorizationRequest = (AuthorizationRequest) model.get("authorizationRequest");
+        String clientId = authorizationRequest.getClientId();
+        model.put("clientId", clientId);
+
+        @SuppressWarnings("unchecked")
+        Map<String, Object> scopes = (Map<String, Object>) (model.containsKey("scopes") ?
+                model.get("scopes") : request.getAttribute("scopes"));
+
+        LOG.debug("dumping scopes key:value");
+        scopes.forEach((key, value) -> LOG.debug(key + ":" + value.toString()));
+        model.put("scopesRefine", scopes);
+        return "approval";
     }
 }
