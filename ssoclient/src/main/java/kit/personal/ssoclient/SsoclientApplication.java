7d77331a9c77713172e0d4eede0c6d9c4b9104e7
kitkwlei <kitkwlei@ias.gov.mo>
2019-07-25 16:10:46 +0800

    extract config to profile to support multiple environment

--- ssoclient/src/main/java/kit/personal/ssoclient/SsoclientApplication.java
+++ ssoclient/src/main/java/kit/personal/ssoclient/SsoclientApplication.java
@@ -2,9 +2,10 @@ package kit.personal.ssoclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SsoclientApplication {
+public class SsoclientApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsoclientApplication.class, args);
