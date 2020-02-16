21421d6227dd42f3d55448ebe10d6cb29f66dc53
wingzero <wingzzz2003@hotmail.com>
2020-02-16 14:54:05 +0800

    set ssomanagement context path, because cookie will ignore port different, if no special context path, all cookie will be overwritten by same site;

--- ssomanagement/spa/vue.config.js
+++ ssomanagement/spa/vue.config.js
@@ -4,4 +4,5 @@ module.exports = {
         proxy: 'http://compiler:8082',
     },
     outputDir: '../src/main/resources/static/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/ssomanagement/' : '/'
 }
