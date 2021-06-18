3d0c9dec4e30a77f8d72aff411644a085202a3b5
wingzero0 <wingzzz2003@hotmail.com>
2021-06-18 11:51:06 +0800

    install element ui

---
+++ ssomanagement/spa3/src/views/Element.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>This is an el element page</h1>
+    <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+    </el-container>
+  </div>
+</template>
